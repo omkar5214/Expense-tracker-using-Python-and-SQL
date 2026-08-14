03d0a9d2fb1b001f6628067571afe819327e34c3
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-08-14 18:06:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root123",
+        password="password_of_MySQL",
         database="expense_tracker"
     )
 
